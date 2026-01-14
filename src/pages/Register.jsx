e5a6bfc161f50b5359a10f5a0d33f418bc61fe91
Nahidul-Islam <mdnahid784055@gmail.com>
2026-01-14 17:36:40 +0600

    added sing up & observer

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { use } from 'react';
 import { Link } from 'react-router';
+import { AuthContext } from '../provider/AuthProvider';
+
 
 const Register = () => {
+  const {createUser,setUser}=use(AuthContext)
+  const handelRegister = (e) => {
+    e.preventDefault()
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log({ name, email, password })
+    createUser(email, password)
+      .then(userCredential => {
+        // Signed up
+        const user = userCredential.user;
+        console.log(user);
+        setUser(user)
+      })
+      .catch(error => {
+        const errorMessage = error.message;
+        alert(errorMessage)
+      });
+  }
   return (
-    <div className="flex justify-center items-center">
+    <form
+      onSubmit={handelRegister}
+      className="flex justify-center items-center"
+    >
       <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
         <div className="card-body">
           <h2 className="text-center font-semibold text-3xl">
@@ -11,19 +35,44 @@ const Register = () => {
           </h2>
           <fieldset className="fieldset">
             <label className="label font-bold">Name</label>
-            <input type="text" className="input" placeholder="Your Name" />
+            <input
+              name="name"
+              type="text"
+              className="input"
+              placeholder="Your Name"
+              required
+            />
             <label className="label font-bold">Photo URL</label>
-            <input type="text" className="input" placeholder="Photo URL" />
+            <input
+              type="text"
+              className="input"
+              placeholder="Photo URL"
+              required
+            />
             <label className="label font-bold">Email address</label>
-            <input type="email" className="input" placeholder="Email" />
+            <input
+              name="email"
+              type="email"
+              className="input"
+              placeholder="Email"
+              required
+            />
             <label className="label">Password</label>
-            <input type="password" className="input" placeholder="Password" />
+            <input
+              name='password'
+              type="password"
+              className="input"
+              placeholder="Password"
+              required
+            />
             <label className="label">
               <input type="checkbox" className="checkbox" />
               Accept Term & Conditions
             </label>
 
-            <button className="btn btn-neutral mt-4">Resister</button>
+            <button type="submit" className="btn btn-neutral mt-4">
+              Resister
+            </button>
           </fieldset>
           <p>
             {' '}
@@ -34,7 +83,7 @@ const Register = () => {
           </p>
         </div>
       </div>
-    </div>
+    </form>
   );
 };
 
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,14 +1,33 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged } from 'firebase/auth';
+import app from '../firebase/firebase.config';
 export const AuthContext = createContext();
-const AuthProvider = ({children}) => {
-  const [user, setUser] = useState({
-    name: 'karim',
-    email:'gorochor@gmail.com'
-  })
+
+const auth = getAuth(app);
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null)
+  
+  //create user
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  }
+
+  //set observer
+  useEffect(() => {
+    onAuthStateChanged(auth, user => {
+      if (user) {
+        console.log(user);
+      } else {
+        console.log('not user');
+      }
+    });
+  }, []);
+
   const authData = {
     user,
-    setUser
-  }
+    setUser,
+    createUser,
+  };
   return <AuthContext value={authData}>{children}</AuthContext>;
 };
 
